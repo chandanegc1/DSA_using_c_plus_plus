0f93d82a06e2628832ea2ef7b7f42bd575a79722
CHANDAN KANAUJIA <113818031+chandanegc1@users.noreply.github.com>
2024-02-06 19:17:46 +0530

    recursion

--- 2.Recursion/1.Recursion.cpp
+++ 2.Recursion/1.Recursion.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+
 void printName(int i ,string n){
     if(i==0)
       return;
@@ -8,10 +10,42 @@ void printName(int i ,string n){
     printName(i-1 ,n);
 }
 
+
+
+int sum(int n , int i=0){
+  if(n==1)
+   {
+      return i;
+  }
+  i+=n;
+  return sum(n-1 ,i);
+}
+
+
+void reverse(int i , int arr[] , int n){
+
+  if(i>=n/2) return;
+  swap(arr[i] ,arr[n-i-1]);
+  return reverse(i+1 , arr ,n);
+}
+
+
+bool palindrom(int i , string s){
+  if(i>=s.size()/2) return true;
+
+  if((s[i]!=s[s.size()-i-1])) return false;
+
+  return palindrom(i+1 , s);
+}
+
+
 int main(){
-    int i;
-    string n;
-    cin>>i;
-    cin>>n;
-    printName(i ,n);
+   int n;
+   cin>>n;
+  //  int arr[n];
+  //  for(int k=0;k<n ;k++){
+  //   cin>>arr[k];
+  // }
+   cout<<palindrom(0 , "aabaa" );
+   
 }
