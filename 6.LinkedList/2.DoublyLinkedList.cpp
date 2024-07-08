9a04918e584d9a234fc13919dc7bf22e8715d9eb
CHANDAN KANAUJIA <113818031+chandanegc1@users.noreply.github.com>
2024-07-08 12:13:04 +0530

    Doubly Linkedlist create , insert element

---
+++ 6.LinkedList/2.DoublyLinkedList.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* pre;
+    Node* next;
+
+    Node(int data){
+        this->data=data;
+        pre=NULL;
+        next=NULL;
+    }
+};
+
+// 1# Traverse a Linked List and Display all elements
+void display(Node* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
+
+// 2# Insert an element in Doubly Linkdelist
+void insertion(Node* &head , int data){
+    Node* cur = new Node(data);
+    if(head==NULL){
+        head=cur;
+        return;
+    }
+
+    if(head->next==NULL){
+        head->next=cur;
+        cur->pre=head;
+        return;
+    }
+
+    Node* temp = head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next= cur;
+    cur->pre=temp;
+}
+
+// 3# Insert element at the nth position
+void insertNthPossition(Node* head, int data , int key){
+
+}
+// 4# Reverse Linked List
+void reverse(Node* head){
+
+}
+// 7# Delete an element from Linkedlist
+void deleteAnElement(Node* &head , int key){
+    
+}
+int main(){
+    Node* head=NULL;
+    insertion(head ,1);
+    insertion(head ,2);
+    insertion(head ,3);
+    insertion(head ,4);
+    display(head);
+    return 0;
+}
