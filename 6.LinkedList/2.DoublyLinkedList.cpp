3f489de9bb06e935afbd82ae342d0c5df2f2dfb2
CHANDAN KANAUJIA <113818031+chandanegc1@users.noreply.github.com>
2024-07-09 15:03:28 +0530

    doubly Linkedlist , insert at nth possition

--- 6.LinkedList/2.DoublyLinkedList.cpp
+++ 6.LinkedList/2.DoublyLinkedList.cpp
@@ -46,11 +46,33 @@ void insertion(Node* &head , int data){
 
 // 3# Insert element at the nth position
 void insertNthPossition(Node* head, int data , int key){
+    
+    Node* cur = new Node(data);
+    if(head==NULL && key==1){
+        head=cur;
+        return;
+    }
 
+    if(head==NULL){
+        return;
+    }
+    Node* temp =head;
+    for(int i = 2 ; temp!=NULL;temp=temp->next){
+        if(key==i){
+            Node* nextLink = temp->next;
+            temp->next=cur;
+            cur->pre=temp;
+            cur->next=nextLink;
+            nextLink->pre = cur;
+            break;
+        }
+        i++;
+    }
 }
+
 // 4# Reverse Linked List
 void reverse(Node* head){
-
+    
 }
 // 7# Delete an element from Linkedlist
 void deleteAnElement(Node* &head , int key){
@@ -62,6 +84,7 @@ int main(){
     insertion(head ,2);
     insertion(head ,3);
     insertion(head ,4);
+    insertNthPossition(head ,81 ,4);
     display(head);
     return 0;
 }
