504854cb69801d03df0fd8576b8ee7a08f07ac74
CHANDAN KANAUJIA <113818031+chandanegc1@users.noreply.github.com>
2024-07-15 13:47:22 +0530

    // 2# find the maximum subarray lenght that is ans>=k

--- 9.TP_SW/1.const_length.cpp
+++ 9.TP_SW/1.const_length.cpp
@@ -18,9 +18,31 @@ int maxSum(int arr[], int n, int k){
 }
 
 // 2# find the maximum subarray lenght that is ans>=k
+void subarray(int arr[], int n, int k) {
+    int l = 0, maxSum = 0, ansR = 0, ansL = 0, sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+        while (sum > k && l <= i) {
+            sum -= arr[l];
+            l++;
+        }
+        if (sum > maxSum && sum <= k) {
+            ansR = i;
+            ansL = l;
+            maxSum = sum;
+        }
+    }
+
+    for (int i = ansL; i <= ansR; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
 int main(){
-    int arr[] = {-1, 2, 3, 3, 4, 5, -1};
-    cout<<maxSum(arr,7,4)<<endl;
+    // int arr[] = {-1, 2, 3, 3, 4, 5, -1};
+    int arr2[] = {6,2,3,4,7,2,1,7,1};
+    // cout<<maxSum(arr,7,4)<<endl;
+    subarray(arr2, 9,14);
     return 0;
 }
